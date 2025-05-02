e40d32aae01da8d85877e9962d67255833514628
ramin-buy002 <204464497+ramin-Buy002@users.noreply.github.com>
2025-05-02 15:43:02 -0400

    addPostSale

--- src/components/addPostSale/AddPostSale.js
+++ src/components/addPostSale/AddPostSale.js
@@ -1,4 +1,8 @@
 import React from 'react'
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import { InputAdornment } from '@mui/material';
+
 
 const AddPostSale = () => {
   return (
@@ -9,10 +13,91 @@ const AddPostSale = () => {
                     
                 
                          
-                    <div  class=" bg-white  w-130  h-100 ml-20   "  >
-                            <span> </span>
-                       
-                        </div>
+                    <div  class=" bg-white  w-83  h-165 ml-200 mb-30 -mt-6  "  >
+                            <div  class=" flex bg-green-300  w-77  h-85 ml-3 mt-3  "  >
+                               <div  > 
+                                  <div  class=" bg-white  w-13  h-13  ml-60 mt-3 " ></div>
+                                  <div  class=" bg-white  w-13  h-13  ml-60 mt-3 " ></div>
+                                  <div  class=" bg-white  w-13  h-13  ml-60 mt-3 " ></div>
+                                  <div  class=" bg-white  w-13  h-13  ml-60 mt-3 " ></div>
+                               </div>
+                            </div>
+                <div>
+                     <Box
+                    component="form"
+                    sx={{ '& .MuiTextField-root': { m: 1, width: '30ch' } }}
+                    noValidate
+                    autoComplete="off"
+                  >
+    
+    
+                      <div>
+                      <TextField
+                          id="standard-multiline-flexible"
+                          label="Caption"
+                          multiline
+                          maxRows={2}
+                          variant="standard"
+                        />
+                      
+                      </div>
+                    
+                
+                     </Box>
+                </div>    
+                <div >
+                     <Box
+                    component="form"
+                    sx={{ '& .MuiTextField-root': { m: 2 , width: '19ch' , height:'4ch' } }}
+                    noValidate
+                    autoComplete="off"
+                  >
+                  
+                      <div   class="flex flex-row">
+                      <TextField
+                          label=""
+                          id="outlined-start-adornment"
+                          sx={{ m: 1, width: '25ch' }}
+                          slotProps={{
+                            input: {
+                              startAdornment: <InputAdornment position="start">price $ : </InputAdornment>
+                            },
+                          }}
+                        />
+                   
+                      </div>
+                      <div   class="flex flex-row">
+                      <TextField
+                          label=""
+                          id="outlined-start-adornment"
+                          sx={{ m: 1, width: '25ch' }}
+                          slotProps={{
+                            input: {
+                              startAdornment: <InputAdornment position="start">price $ : </InputAdornment>
+                            },
+                          }}
+                        />
+                   
+                      </div>
+                      <div   class="flex flex-row">
+                      <TextField
+                          label=""
+                          id="outlined-start-adornment"
+                          sx={{ m: 1, width: '25ch' }}
+                          slotProps={{
+                            input: {
+                              startAdornment: <InputAdornment position="start">price $ : </InputAdornment>
+                            },
+                          }}
+                        />
+                   
+                      </div>
+                        
+                      
+                
+                     </Box>
+                </div>  
+                    </div>
                  
                     </div>
                     
