5f5cdb3bb20c9e99e83fd8a95014d2eefaaaf2ef
ramin-buy002 <204464497+ramin-Buy002@users.noreply.github.com>
2025-05-07 17:14:50 -0400

    addPostSale

--- src/components/addPostSale/AddPostSale.js
+++ src/components/addPostSale/AddPostSale.js
@@ -12,13 +12,13 @@ const AddPostSale = () => {
  
   return (
     <div class="bg-sky-950  ">
-            <div className=" ml-58 bg-gray-100 rounded-l-3xl">
+            <div className=" ml-58 bg-gray-50 rounded-l-3xl">
              <h3> ..</h3>
                     <div className=" flex row">
                     
                 
                          
-                    <div  class=" bg-white  w-83  h-168 ml-200 mb-30 -mt-6  "  >
+                    <div  class=" bg-white  shadow-lg w-83  h-168 ml-200 mb-30 -mt-6  "  >
                             <div  class=" flex bg-green-300  w-77  h-85 ml-3 mt-3  "  >
                                <div  > 
                                   <div  class=" bg-white  w-13  h-13  ml-60 mt-3 " ></div>
@@ -36,21 +36,17 @@ const AddPostSale = () => {
                   >
     
     
-                      <div>
-                      <TextField
-                          id="standard-multiline-flexible"
-                          label="Caption"
-                          multiline
-                          maxRows={2}
-                          variant="standard"
-                        />
+                      <div class="ml-6 mt-3">
+                       <span  class="text-blue-400">
+                        Caption :  
+                       </span>
                       
                       </div>
                     
                 
                      </Box>
                 </div>    
-                <div >
+                <div class="ml-1 mt-10" >
                      <Box
                     component="form"
                     // sx={{ '& .MuiTextField-root': { m: 2 , width: '19ch' , height:'3ch' } }}
@@ -61,6 +57,10 @@ const AddPostSale = () => {
                       <div   class="flex flex-row ml-3">
                       <TextField
                           label="offer_01"
+                          inputProps={{
+                            style: { fontSize: 14  }
+                        }}
+                          placeholder=" price ..."
                           id="outlined-start-adornment"
                           sx={{ m: 1, width: '12ch' , "& .MuiOutlinedInput-input":{height:10 },
                           fontSize:22
@@ -70,18 +70,22 @@ const AddPostSale = () => {
                             input: {
                               startAdornment: <InputAdornment 
                               sx={{fontSize:15}}
-                              position="start" >$ : </InputAdornment>
+                              position="start" ></InputAdornment>
                             },
                           }}
                         />
                          <TextField
                           label=""
+                          inputProps={{
+                            style: { fontSize: 15  }
+                        }}
                           id="outlined-start-adornment"
+                          placeholder=" pcs ..."
                           sx={{ m: 1, width: '14ch' , "& .MuiOutlinedInput-input":{height:10}
                            }}
                           slotProps={{
                             input: {
-                              startAdornment: <InputAdornment position="start">pcs : </InputAdornment>
+                              startAdornment: <InputAdornment position="start"> </InputAdornment>
                             },
                           }}
                         />
@@ -91,6 +95,10 @@ const AddPostSale = () => {
                       <div   class="flex flex-row ml-3">
                       <TextField
                           label="offer_02"
+                          inputProps={{
+                            style: { fontSize: 15  }
+                        }}
+                          placeholder=" price ..."
                           id="outlined-start-adornment"
                           sx={{ m: 1, width: '12ch' , "& .MuiOutlinedInput-input":{height:10 },
                           fontSize:22
@@ -100,18 +108,22 @@ const AddPostSale = () => {
                             input: {
                               startAdornment: <InputAdornment 
                               sx={{fontSize:15}}
-                              position="start" >$ : </InputAdornment>
+                              position="start" > </InputAdornment>
                             },
                           }}
                         />
                          <TextField
                           label=""
+                          inputProps={{
+                            style: { fontSize: 15  }
+                        }}
+                          placeholder=" pcs ..."
                           id="outlined-start-adornment"
                           sx={{ m: 1, width: '14ch' , "& .MuiOutlinedInput-input":{height:10}
                            }}
                           slotProps={{
                             input: {
-                              startAdornment: <InputAdornment position="start">pcs : </InputAdornment>
+                              startAdornment: <InputAdornment position="start"> </InputAdornment>
                             },
                           }}
                         />
@@ -120,6 +132,10 @@ const AddPostSale = () => {
                       <div   class="flex flex-row ml-3">
                       <TextField
                           label="offer_03"
+                          inputProps={{
+                            style: { fontSize: 15  }
+                        }}
+                          placeholder=" price ..."
                           id="outlined-start-adornment"
                           sx={{ m: 1, width: '12ch' , "& .MuiOutlinedInput-input":{height:10 },
                           fontSize:22
@@ -129,25 +145,29 @@ const AddPostSale = () => {
                             input: {
                               startAdornment: <InputAdornment 
                               sx={{fontSize:15}}
-                              position="start" >$ : </InputAdornment>
+                              position="start" > </InputAdornment>
                             },
                           }}
                         />
                          <TextField
                           label=""
+                          inputProps={{
+                            style: { fontSize: 15  }
+                        }}
+                          placeholder=" pcs ..."
                           id="outlined-start-adornment"
                           sx={{ m: 1, width: '14ch' , "& .MuiOutlinedInput-input":{height:10}
                            }}
                           slotProps={{
                             input: {
-                              startAdornment: <InputAdornment position="start">pcs : </InputAdornment>
+                              startAdornment: <InputAdornment position="start"> </InputAdornment>
                             },
                           }}
                         />
                    
                       </div>
                   
-                      <div  class="ml-29" >
+                      <div  class="ml-27 mt-1" >
 
                               <Tooltip title="Click to see loading">
                                                             <Button  onClick={() => setLoading(true)} loading={loading}
