d858629bca381b215082e0802b9a65c2f3544af9
ramin-buy002 <204464497+ramin-Buy002@users.noreply.github.com>
2025-09-30 00:16:57 -0400

    Update CryptoPrice.jsx

--- src/components/crypto/CryptoPrice.jsx
+++ src/components/crypto/CryptoPrice.jsx
@@ -9,8 +9,8 @@ const CryptoPrice = () => {
 const [XRP , setBuyXRP ] = useState(2.87590) ;
 const [DOGE , setBuyDOGE ] = useState( 0.23837 ) ;
 const [LINK , setBuyLINK ] = useState(21.16) ;
-const [NEAR , setBuyNEAR ] = useState(2.772) ;
-const [ADA , setBuyADA ] = useState(0.7978 ) ;
+const [NEAR , setBuyNEAR ] = useState(2.720 ) ;
+const [ADA , setBuyADA ] = useState(0.7971 ) ;
 const [AVAX , setBuyAVAX ] = useState(29.880) ;
 const [HYPE , setBuyHYPE ] = useState(45.06 ) ;
 
